321f837514f8be8ae92b31287a01c5530bec6131
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-12-03 12:22:51 +0530

    Create 424. Longest Repeating Character Replacement.py

---
+++ Questions/424. Longest Repeating Character Replacement.py
@@ -0,0 +1,47 @@
+Given a string s that consists of only uppercase English letters, you can perform at most k operations on that string.
+
+In one operation, you can choose any character of the string and change it to any other uppercase English character.
+
+Find the length of the longest sub-string containing all repeating letters you can get after performing the above operations.
+
+Note:
+Both the string's length and k will not exceed 104.
+
+Example 1:
+
+Input:
+s = "ABAB", k = 2
+
+Output:
+4
+
+Explanation:
+Replace the two 'A's with two 'B's or vice versa.
+ 
+
+Example 2:
+
+Input:
+s = "AABABBA", k = 1
+
+Output:
+4
+
+Explanation:
+Replace the one 'A' in the middle with 'B' and form "AABBBBA".
+The substring "BBBB" has the longest repeating letters, which is 4.
+
+
+from collections import Counter
+def Max_string(s,k):
+    s= list(s)
+    pt1=0
+    pt2=0
+    d = Counter(s)
+    for i in range(len(s)):
+        
+
+
+
+
+
