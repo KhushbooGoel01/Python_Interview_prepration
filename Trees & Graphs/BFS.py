c6fd7ce4c6f8c952911b243c61a30e2703566d2e
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-12-04 21:05:32 +0530

    Create BFS.py

---
+++ Trees & Graphs/BFS.py
@@ -0,0 +1,20 @@
+graph = {
+    'A' : ['B','C'],
+    'B' : ['D', 'E'],
+    'C' : ['F'],
+    'D' : [],
+    'E' : [],
+    'F' : []
+}
+
+visited = set() # Set to keep track of visited nodes.
+
+def dfs(visited, graph, node):
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+dfs(visited, graph, 'A')
