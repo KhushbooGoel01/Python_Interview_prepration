92a31aef388db4a77380bbbea311816526c0320d
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-12-07 13:54:30 +0530

    Create convert_max.py

---
+++ Heap_Questions/convert_max.py
@@ -0,0 +1,8 @@
+Problem Statement 
+
+Implement a function convertMax(vector<int> maxHeap), which will convert a binary Max-Heap into a binary Min-Heap. Where maxHeap is a vector which is given in the maxHeap form, i.e., the parent is greater than its children.
+
+Output 
+Returns converted list in string format
+
+
