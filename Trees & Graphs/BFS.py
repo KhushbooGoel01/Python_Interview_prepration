d834959ac238d7d43739a25b2d0f256e13fabf13
Khushboo Goel <59544806+KhushbooGoel01@users.noreply.github.com>
2020-12-09 00:53:30 +0530

    Update BFS.py

--- Trees & Graphs/BFS.py
+++ Trees & Graphs/BFS.py
@@ -1,20 +1,29 @@
+
+
 graph = {
-    'A' : ['B','C'],
-    'B' : ['D', 'E'],
-    'C' : ['F'],
-    'D' : [],
-    'E' : [],
-    'F' : []
+  'A' : ['B','C'],
+  'B' : ['D', 'E'],
+  'C' : ['F'],
+  'D' : [],
+  'E' : ['F'],
+  'F' : []
 }
 
-visited = set() # Set to keep track of visited nodes.
+visited = [] # List to keep track of visited nodes.
+queue = []     #Initialize a queue
 
-def dfs(visited, graph, node):
-    if node not in visited:
-        print (node)
-        visited.add(node)
-        for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+def bfs(visited, graph, node):
+  visited.append(node)
+  queue.append(node)
+
+  while queue:
+    s = queue.pop(0) 
+    print (s, end = " ") 
+
+    for neighbour in graph[s]:
+      if neighbour not in visited:
+        visited.append(neighbour)
+        queue.append(neighbour)
 
 # Driver Code
-dfs(visited, graph, 'A')
+bfs(visited, graph, 'A')
